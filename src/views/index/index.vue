fc883416655f2c7a6b4945da2ac290ddeb14468e
xiranran <2449500848@qq.com>
2023-08-08 15:32:16 +0800

    feat: 汇总页面+ 添加饼状图
    汇总页面+ 添加饼状图
    
    BREAKING CHANGE:
    汇总页面+ 添加饼状图
    
    ISSUES CLOSED: #1

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,4 +1,57 @@
 <template>
-  <div></div>
+  <div class="w-[100%] mt-[80px] ml-[30px]">
+    <div id="main"></div>
+  </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import * as echarts from "echarts";
+let initPriority = () => {
+  let myChart = echarts.init(document.getElementById("main"));
+  const option = {
+    title: {
+      text: "Referer of a Website",
+      subtext: "Fake Data",
+      left: "center",
+    },
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      orient: "vertical",
+      left: "left",
+    },
+    series: [
+      {
+        name: "Access From",
+        type: "pie",
+        radius: "50%",
+        data: [
+          { value: 1048, name: "Search Engine" },
+          { value: 735, name: "Direct" },
+          { value: 580, name: "Email" },
+          { value: 484, name: "Union Ads" },
+          { value: 300, name: "Video Ads" },
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      },
+    ],
+  };
+  myChart.setOption(option);
+};
+onMounted(() => {
+  initPriority();
+});
+</script>
+<style>
+#main,
+#priority {
+  width: 450px;
+  height: 300px;
+}
+</style>
